4a880a51145d59a2088c4dc6126680d9bd14f86f
Kenny <kenny_hung@mail.chungyo.net>
2022-09-22 14:54:24 +0800

    解決無法引用 element ui 樣式檔案的問題

--- tampermonkey.js
+++ tampermonkey.js
@@ -2,8 +2,6 @@
 // @name         __APP_NAME__
 // @namespace    http://tampermonkey.net/
 // @version      __APP_VERSION__
-// @description  使用Vue2.x element-ui构建油猴TamperMonkey插件
-// @author       harry
 // @run-at       document-start
 // @grant        GM_getValue
 // @grant        GM_setValue
@@ -15,8 +13,12 @@
 // @grant        GM.setClipboard
 // @grant        GM_info
 // @grant        GM.xmlHttpRequest
+// @grant        GM_getResourceText
+// @grant        GM_addStyle
+// @resource     IMPORTED_CSS https://unpkg.com/element-ui/lib/theme-chalk/index.css
 // @require      https://cdn.bootcdn.net/ajax/libs/vue/2.6.12/vue.min.js
 // @require      https://unpkg.com/element-ui@2.14.1/lib/index.js
 // @require      https://cdn.bootcdn.net/ajax/libs/axios/0.21.0/axios.min.js
-// @include      *
+// @match        *
 // ==/UserScript==
+const my_css = GM_getResourceText("IMPORTED_CSS");GM_addStyle(my_css);
